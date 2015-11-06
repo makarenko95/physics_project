3571282ac310c011a1a4b3e387c892fcf5847ffa
Vladimir Makarenko <vlamakarenko@gmail.com>
2015-11-06 12:12:03 +0300

    architecture has been improved

--- application.cpp
+++ application.cpp
@@ -4,74 +4,10 @@
 #include <Control/qtbilliardcontrol.h>
 
 Application::Application(QWidget *parent)
-    : QWidget(parent), model({2.0,500,300,20.0,0.0,150.0,50.0}), interval(15)
+    : QWidget(parent), controller(&model, &view)
 {
-    controller.SetParams({2.0,500,300,20.0,0.0,150.0,50.0});
-    view = new QtBilliardView;
-    model.AddView(*view);
     QHBoxLayout * layout = new QHBoxLayout;
-    layout->addWidget(view);
-    QtBilliardControl * control = new QtBilliardControl(this);
-    layout->addWidget(control);
+    layout->addWidget(&view);
+    layout->addWidget(&controller);
     setLayout(layout);
-    timer = new QTimer;
-    connect(timer, SIGNAL(timeout()), this, SLOT(onTimeOut()));
-    timer->start(interval);
-}
-
-Application::~Application()
-{
-    delete timer;
-}
-
-void Application::onTimeOut()
-{
-    model.update(interval * 1.0 / 1000);
-}
-void Application::buttonReloadPressed()
-{
-    if(timer->isActive())
-        timer->stop();
-    BilliardModelParams params = controller.GetParams();
-    model.Reload(&params);
-    timer->start(interval);
-}
-void Application::buttonStopTimerPressed()
-{
-    if(timer->isActive()){
-        timer->stop();
-    }
-    else timer->start(interval);
-}
-void Application::radiusChanged(double radius)
-{
-    controller.SetParticleRadius(radius);
-}
-void Application::sizeChanged(int size)
-{
-    controller.SetBoxSize(size);
-}
-void Application::particlenumChanged(int num)
-{
-    controller.SetMaxParticleNumber(num);
-}
-void Application::particleVelocityChanged(double vel)
-{
-    controller.SetMaxParticleVelocity(vel);
-}
-void Application::pistonPathChanged(double path)
-{
-    controller.SetPistonEndPosition(path);
-}
-void Application::pistonVelocityChanged(double vel)
-{
-    controller.SetPistonVelocity(vel);
-}
-void Application::changeViewerSettings_trace()
-{
-    model.changeViewerSettings_DrawTrace();
-}
-void Application::changeViewerSettings_particles()
-{
-    model.changeViewerSettings_DrawParticles();
 }
--- application.h
+++ application.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <Models/billiardmodel.h>
 #include <Views/qtbilliardview.h>
+#include <Control/qtbilliardcontrol.h>
 #include <QTimer>
 
 class Application : public QWidget
@@ -12,26 +13,13 @@ class Application : public QWidget
 
 public:
     Application(QWidget *parent = 0);
-    ~Application();
+
 private:
     BilliardModel model;
-    BilliardControl controller;
-    QtBilliardView *view;
-    QTimer * timer;
-    int interval;
+    QtBilliardView view;
+    QtBilliardControl controller;
 
 public slots:
-    void onTimeOut();
-    void buttonReloadPressed();
-    void buttonStopTimerPressed();
-    void radiusChanged(double);
-    void sizeChanged(int);
-    void particlenumChanged(int);
-    void particleVelocityChanged(double);
-    void pistonPathChanged(double);
-    void pistonVelocityChanged(double);
-    void changeViewerSettings_trace();
-    void changeViewerSettings_particles();
 };
 
 #endif // APPLICATION_H
